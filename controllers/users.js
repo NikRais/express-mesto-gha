67e389799f32b8cf6ec0dd868a83b1273e378cb7
Nikita Raisky <mikado.raisky@yandex.ru>
2023-10-12 13:51:07 +0500

    13 PR 1.4

--- controllers/users.js
+++ controllers/users.js
@@ -1,35 +1,38 @@
-const { ValidationError, CastError } = require('mongoose').Error;
+const { CastError } = require('mongoose').Error;
 const User = require('../models/user');
-const {
-  errorsResponse, OK_REQUEST, NOT_FOUND, BAD_REQUEST, INTERNAL_SERVER_ERROR,
-} = require('../utils/serverResponse');
+const errorCode = require('../utils/serverResponse');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(OK_REQUEST).send(users))
-    .catch((err) => errorsResponse(err, res));
+    .then((users) => res.status(errorCode.OK_REQUEST).send(users))
+    .catch((err) => errorCode.errorsResponse(err, res));
 };
 
 module.exports.getUsersById = (req, res) => {
   User.findById(req.params.userId)
     .orFail(new Error('NotFound'))
-    .then((user) => res.status(OK_REQUEST).send(user))
+    .then((user) => res.status(errorCode.OK_REQUEST).send(user))
     .catch((error) => {
       if (error.message === 'NotFound') {
-        return res.status(NOT_FOUND).send({ message: 'Пользователь по id не найден' });
+        return res.status(errorCode.NOT_FOUND).send({ message: 'Пользователь по id не найден' });
       }
       if (error instanceof CastError) {
-        return res.status(BAD_REQUEST);
+        return res.status(errorCode.BAD_REQUEST);
       }
-      return res.status(INTERNAL_SERVER_ERROR);
+      return res.status(errorCode.INTERNAL_SERVER_ERROR);
     });
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then((user) => res.status(200).send(user))
-    .catch((err) => errorsResponse(err, res));
+    .then((user) => res.status(errorCode.OK_REQUEST).send({
+      _id: user._id,
+      name: user.name,
+      about: user.about,
+      avatar: user.avatar,
+    }))
+    .catch((err) => errorCode.errorsResponse(err, res));
 };
 
 module.exports.updateUserProfile = (req, res) => {
@@ -39,8 +42,8 @@ module.exports.updateUserProfile = (req, res) => {
     { name, about },
     { new: true, runValidators: true },
   )
-    .then((user) => res.status(OK_REQUEST).send(user))
-    .catch((err) => errorsResponse(err, res));
+    .then((user) => res.status(errorCode.OK_REQUEST).send(user))
+    .catch((err) => errorCode.errorsResponse(err, res));
 };
 
 module.exports.updateUserAvatar = (req, res) => {
@@ -50,6 +53,6 @@ module.exports.updateUserAvatar = (req, res) => {
     { avatar },
     { new: true, runValidators: true },
   )
-    .then((user) => res.status(OK_REQUEST).send(user))
-    .catch((err) => errorsResponse(err, res));
+    .then((user) => res.status(errorCode.OK_REQUEST).send(user))
+    .catch((err) => errorCode.errorsResponse(err, res));
 };
--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const {
 } = require('../controllers/users');
 
 userRouter.get('/', getUsers);
-userRouter.get('/:id', getUsersById);
+userRouter.get('/:userId', getUsersById);
 userRouter.post('/', createUser);
 userRouter.patch('/me', updateUserProfile);
 userRouter.patch('/me/avatar', updateUserAvatar);
