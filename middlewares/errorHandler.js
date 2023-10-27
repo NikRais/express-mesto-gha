ffb6f6fb6c51150ce7c1fbac8231a4ab996cf00d
Nikita Raisky <mikado.raisky@yandex.ru>
2023-10-27 14:19:05 +0500

    14 PR fix text for internal server error

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,7 +2,7 @@ const serverResponse = require('../utils/serverResponse');
 
 module.exports = (err, req, res, next) => {
   res.status(err.statusCode).send({
-    message: err.statusCode === serverResponse.INTERNAL_SERVER_ERROR ? 'Ошибка на стороне сервера' : err.message,
+    message: err.statusCode === serverResponse.INTERNAL_SERVER_ERROR ? 'На сервере произошла ошибка' : err.message,
   });
   return next();
 };
